96cf231e765715976841343c60917a1e7c84e5c3
Duarte Madueño <duarte.madueno@amaris.com>
2014-12-22 17:28:46 +0000

    mouse wheel typescript

---
+++ MouseWheelDirective.ts
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+export function ngMousewheel($timeout: ng.ITimeoutService): ng.IDirective {
+    return {
+        restrict: 'A',
+        scope: { method: '&ngMousewheel'},
+        link: ($scope: any, elem: JQuery, attributes: any) => {
+            console.log(attributes);
+            var element: HTMLElement = elem[0];
+            element.onmousewheel = () => {
+                $scope.method();
+            };
+                
+        }
+    };
+}
+ 
